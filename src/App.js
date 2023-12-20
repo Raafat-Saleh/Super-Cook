c6e90f151c40f0e77be31f84aaff12b8fe7d8c35
Raafat-Saleh <rafat.saleh1313@gmail.com>
2023-12-20 12:40:33 +0200

    edit

--- src/App.js
+++ src/App.js
@@ -37,6 +37,7 @@ function App() {
       })
       .catch((e) => {
         setSearchLoading(false);
+        console.log(e);
         setSearchError("Somthing wrong was happen");
       });
   };
--- src/components/recipe/Recipe.js
+++ src/components/recipe/Recipe.js
@@ -30,6 +30,7 @@ export default function Recipe() {
       })
       .catch((e) => {
         setLoading(false);
+        console.log(e);
         setError(e);
       });
   }, [id]);
